0b6289fe39eb80b60b75f8869e3f783671e02e7d
fmsooyung <91471733+fmsooyung@users.noreply.github.com>
2021-10-24 22:25:58 +0900

    Add files via upload

---
+++ Payment.py
@@ -0,0 +1,7 @@
+price=int(input('料金を入力してください'))
+number=int(input('人数を入力してくだいさい'))
+payment = int(price / number)
+print('お支払いは{}円です'.format(payment))
+
+
+/Users/houmoe/PycharmProjects/pythonProject/Payment.py
